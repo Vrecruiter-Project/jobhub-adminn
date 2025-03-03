305627f0e428eee0d5e4d4c85a6256711ad1444f
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-03-03 17:39:15 +0530

    fix: update CORS origin to use the production URL for frontend

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://jobhubadmin.vercel.app" || "http://localhost:5173",
     credentials: true, // Allow cookies
   })
 );
