77f618566be44d0a0d0205bfee89e3147e7ccab7
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-03-04 17:53:49 +0530

    Uncomment Approuter in App component and clean up Admin dashboard code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <>
-      {/* <Approuter /> */}
-      <Dashboard />
+      <Approuter />
+   
     </>
   )
 }
--- client/src/Pages/Dashboard/Admindashboard.jsx
+++ client/src/Pages/Dashboard/Admindashboard.jsx
@@ -16,8 +16,7 @@ const Dashboard = () => {
   return (
     <>
       <DashboardLayoutBasic />
-      {/* Hello i am dashboard <br />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer" onClick={handleLogout}>Logout</button> */}
+
     </>
   )
 };
