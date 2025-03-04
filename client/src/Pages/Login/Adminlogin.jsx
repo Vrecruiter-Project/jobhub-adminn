5fe07e24e255db4cb013b6641d1e508bad4cf444
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-03-04 15:25:15 +0530

    Refactor Admin login page layout and styling; enhance user experience with background image

--- client/src/Pages/Login/Adminlogin.jsx
+++ client/src/Pages/Login/Adminlogin.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { Container, TextField, Button, Typography, Paper } from "@mui/material";
 import { BASE_URL } from "../../api/api";
 import { useNavigate } from "react-router-dom";
+import bg from "../../assets/SigninBG.jpg"
 
 const Adminlogin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false); 
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
@@ -35,42 +37,79 @@ const Adminlogin = () => {
   };
 
   return (
-    <Container maxWidth="xs">
-      <Paper elevation={10} sx={{ p: 3, mt: 10, textAlign: "center" }}>
-        <Typography variant="h5" gutterBottom>
-          Admin Login
-        </Typography>
-        <form onSubmit={handleLogin}>
-          <TextField
-            label="Username"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            label="Password"
-            type="password"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-            sx={{ mt: 2 }}
-            disabled={loading} // Disable button while loading
-          >
-            {loading ? "Logging in..." : "Login"}
-          </Button>
-        </form>
-      </Paper>
-    </Container>
+
+    <div style={{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+    }} className=" rounded-lg py-2">
+      <div className="container flex flex-col mx-auto bg-grey-200 rounded-lg  my-2 ">
+        <div className="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5">
+          <div className="flex items-center justify-center w-full lg:p-12">
+            <div className="flex items-center xl:p-10">
+              <form onSubmit={handleLogin} className="flex flex-col w-full h-full pb-6 text-center bg-white p-6  rounded-3xl">
+                <h3 className="mb-3 text-4xl font-extrabold text-dark-grey-900">Admin Sign In</h3>
+
+
+                <label htmlFor="email" className="mb-2 mt-8 text-sm text-start text-grey-900">username*</label>
+                <input value={username}
+                  onChange={(e) => setUsername(e.target.value)} placeholder="Enter a username" className="bg-amber-100 flex items-center w-full px-5 py-4 mb-7 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" />
+                <label htmlFor="password" className="mb-2 text-sm text-start text-grey-900">Password*</label>
+                <input value={password}
+                  onChange={(e) => setPassword(e.target.value)} id="password" type="password" placeholder="Enter your password" className="bg-amber-100 flex items-center w-full px-5 py-4 mb-5 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl" />
+            
+
+                <button disabled={loading} className="w-full flex items-center bg-green-400  px-5 py-4 mt-5 text-sm font-medium outline-none focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl text-center justify-center md:w-96 cursor-pointer "> {loading ? "Signin in..." : "Sign In"}</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //       <Button
+    //         type="submit"
+    //         variant="contained"
+    //         color="primary"
+    //         fullWidth
+    //         sx={{ mt: 2 }}
+    //         disabled={loading} // Disable button while loading
+    //       >
+    //         {loading ? "Logging in..." : "Login"}
+    //       </Button>
+    //     </form>
+    //   </Paper>
+    // </Container>
   );
 };
 
--- server/app.js
+++ server/app.js
@@ -1,34 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// const app = express();
-
-// const dotenv = require("dotenv");
-// dotenv.config();
-// app.use(cookieParser());
-// //middleware
-// app.use(bodyParser.json());
-// app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: ["https://jobhubadmin.vercel.app/","http://localhost:5173"],
-//     credentials: true, // Allow cookies
-//   })
-// );
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-
-// //routes
-
-// //for admin
-// const adminRoute = require("./src/routes/admin.route");
-
-// app.use("/api/admin", adminRoute); //admin login
-
-// module.exports = app;
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
