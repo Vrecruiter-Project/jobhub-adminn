e44e3a0dc8c89d65dd41b589125b44f2d0c70417
w3cdpass <killu1875@gmail.com>
2025-03-05 12:23:32 +0530

    dash

--- client/src/Pages/Dashboard/Admindashboard.jsx
+++ client/src/Pages/Dashboard/Admindashboard.jsx
@@ -1,21 +1,22 @@
-import React from "react";
+import AppProviderBasic from "./component/Sidebar/Sidebar";
 import { BASE_URL } from "../../api/api";
+const handleLogout = async () => {
+  try {
+    await fetch(`${BASE_URL}/api/admin/logout`, { method: "POST", credentials: "include" });
+    window.location.href = "/";
+  } catch (error)  {
+    console.error("Logout failed:", error);
+  }
+};
 
 const Dashboard = () => {
 
-  const handleLogout = async () => {
-    try {
-      await fetch(`${BASE_URL}/api/admin/logout`, { method: "POST", credentials: "include" });
-      window.location.href = "/";
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-  };
+  
 
   return (
     <>
-      <h1>Dashboard</h1>
-      <button onClick={handleLogout}>Logout</button>
+      {/* <AppProviderBasic /> */}
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer" onClick={handleLogout}>Logout</button>
     </>
   )
 };
