7b29bde36f49bbffa610086f42f1912ea14c956e
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-03-11 12:08:18 +0530

    refactor: clean up imports in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import AppProviderBasic from './Pages/Dashboard/component/Sidebar/Sidebar'
-// import Approuter from './routes/Approuter'
+//import AppProviderBasic from './Pages/Dashboard/component/Sidebar/Sidebar'
+import Approuter from './routes/Approuter'
 // import { OffLine } from '../utils/Error'
 function App() {
 
